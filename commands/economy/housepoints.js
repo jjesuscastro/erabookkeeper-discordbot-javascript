8c75482facb2643e3b31caecb4918f5df587d305
faeruumi <faeruumi@gmail.com>
2026-07-12 23:36:13 +0800

    Update housepoints.js

--- commands/economy/housepoints.js
+++ commands/economy/housepoints.js
@@ -16,7 +16,7 @@ module.exports = {
             const points = await getHousePoints(input);
 
             const embed = new EmbedBuilder()
-                .setTitle(`${input} House Points`)
+                .setTitle(`${input.charAt(0).toUpperCase()} House Points`)
                 .setColor(0xB7B75F)
                 .setDescription(`\`\`\`✧ ${points} points ✧\`\`\``);
 
