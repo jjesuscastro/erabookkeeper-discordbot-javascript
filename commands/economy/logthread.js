b34367284529d862ed7197378d8186f7a805af03
faeruumi <faeruumi@gmail.com>
2026-08-01 20:27:33 +0800

    Update logthread.js

--- commands/economy/logthread.js
+++ commands/economy/logthread.js
@@ -232,10 +232,10 @@ module.exports = {
                 .addFields(
                     { name: '',      value: '**Start: **'+startInput      || '—', inline: true },
                     { name: '', value: `**Total WC: **`+totalWords.toString()+' words' || '—', inline: true },
-                    { name: '\u200b', value: '\u200b', inline: true },
+                    { name: '', value: '', inline: true },
                     { name: '', value: '**End: **'+endInput || '—', inline: true },
                     { name: '', value: '**Total Messages: **'+messages.length.toString()+' messages' || '—', inline: true },
-                    { name: '\u200b', value: '\u200b', inline: true },
+                    { name: '', value: '', inline: true },
                 )
                 .setDescription(`\n ${description}`);
 
