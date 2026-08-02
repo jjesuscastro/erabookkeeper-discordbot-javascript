dd6f1b510bbdf5e2cd9e540f2d8e9078db8448cb
faeruumi <faeruumi@gmail.com>
2026-08-02 22:01:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
     ],
 });
 client.commands = new Collection();
