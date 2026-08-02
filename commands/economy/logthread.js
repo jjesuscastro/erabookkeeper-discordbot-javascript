209caddb574473d9ad0529d701a27ea804ab74b1
faeruumi <faeruumi@gmail.com>
2026-08-03 02:34:26 +0800

    Update logthread.js

--- commands/economy/logthread.js
+++ commands/economy/logthread.js
@@ -229,7 +229,7 @@ module.exports = {
 
             for (const result of results){
                 result.userId = await getUserID(result.name);
-                if(!result.userId){
+                if(result.userId === null){
                     members.find(m => m.displayName === result.name).id;
                     result.registered = false;
                 }
