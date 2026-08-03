726fb5331712a56d19ff598be853bdb9f2616d32
faeruumi <faeruumi@gmail.com>
2026-08-03 13:26:32 +0800

    Update logthread.js

--- commands/economy/logthread.js
+++ commands/economy/logthread.js
@@ -231,7 +231,7 @@ module.exports = {
             const edelpay = new Array();
             for (const result of results){
                 result.userId = await getUserID(result.name);
-                if(result.userId){
+                if(result.userId !== null){
                     lines.push(`\`${result.words.toString().padEnd(parseInt(results[0].words.toString().length), " ")} WC\` — **${result.name}** — <@${result.userId}>`);
                     edelpay.push(`\`${result.edels.toString().padEnd(parseInt(results[0].edels.toString().length), " ")} edels\` — <@${result.userId}>`);
                 }
