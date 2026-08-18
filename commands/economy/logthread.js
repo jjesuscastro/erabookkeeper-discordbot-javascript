327d0d73f212326fd67ab05f8df241b664a94ba8
faeruumi <faeruumi@gmail.com>
2026-08-19 00:21:25 +0800

    Update logthread.js

--- commands/economy/logthread.js
+++ commands/economy/logthread.js
@@ -511,7 +511,7 @@ module.exports = {
                         .setColor(0xEBBCA2)
                         .setDescription(`See you next time!`);
 
-                    await interaction.followUp({ embeds: [cancelembed], ephemeral: true });
+                    await interaction.followUp({ embeds: [cancelembed] });
                     return;
                 } 
                 let reviewChannel;
