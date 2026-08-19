3c549ce027466d52b87900e2f188099d0577d7b5
faeruumi <faeruumi@gmail.com>
2026-08-20 00:17:26 +0800

    Update allbalances.js

--- commands/economy/allbalances.js
+++ commands/economy/allbalances.js
@@ -17,7 +17,7 @@ module.exports = {
             const embed = new EmbedBuilder()
                 .setTitle('All Balances')
                 .setColor(0xB7B75F)
-                .setDescription(items.map((i, index ) => `**${index+1}**. **${i.character}** — *${i.balance} edels*`).join('\n'));
+                .setDescription(items.map((i, index ) => `${index+1}. **${i.character}** — *${i.balance} edels*`).join('\n'));
             await interaction.editReply({ embeds: [embed] });
         } catch (err) {
             await interaction.editReply(`Error: ${err.message}`);
