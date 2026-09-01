a3be0c134725db181b28b2c8444766d4d2f6aea1
faeruumi <faeruumi@gmail.com>
2026-09-02 02:04:39 +0800

    Update shop.js

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -20,8 +20,14 @@ module.exports = {
             const embed = new EmbedBuilder()
                 .setTitle('Eirenhel Services')
                 .setColor(0xB7B75F)
-                .setDescription(items.map(i => `**${i.name}** — *${i.price} edels* \n > ${i.itemdesc} \n`).join('\n'))
-                .setFooter(`Every shop item is a one time \`/use\``);
+                .setDescription(items.map(i => !(i.name.includes('Potion')) ? `**${i.name}** — *${i.price} edels* \n > ${i.itemdesc} \n` : '').join('\n'))
+                .setFooter({text:`Every shop item is a one time \`/use\``});
+
+            const embed1 = new EmbedBuilder()
+                .setColor(0xB7B75F)
+                .setDescription(items.map(i => i.name.includes('Potion') ? `**${i.name}** — *${i.price} edels* \n > ${i.itemdesc} \n` : '').join('\n'))
+                //.setDescription(items.map(i => `**${i.name}** — *${i.price} edels* \n > ${i.itemdesc} \n`).join('\n'))
+                .setFooter({text:`Every shop item is a one time \`/use\``});
 
             await interaction.editReply({ embeds: [embed] });
         } catch (err) {
