0480fae130afe13d94578d34bd6ff17c92d11bce
faeruumi <faeruumi@gmail.com>
2026-09-10 15:30:15 +0800

    Update dice.js

--- commands/economy/dice.js
+++ commands/economy/dice.js
@@ -26,7 +26,7 @@ module.exports = {
                 const rolls2 = [];
                 var total = 0;
 
-                if(numDice == null){
+                if(numDice == null || numDice == ''){
                     numDice = 1;
                 }
                 for (let i = 0; i < numDice; i++) {
